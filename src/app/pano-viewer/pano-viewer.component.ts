12761ac33d646d61bb9bc88a0d942f67bddfe796
p_0g_8mm3_ <lachenmon@web.de>
2019-04-05 20:09:29 +0200

    split up pano

--- src/app/pano-viewer/pano-viewer.component.ts
+++ src/app/pano-viewer/pano-viewer.component.ts
@@ -12,9 +12,23 @@ export class PanoViewerComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    pannellum.viewer('pano', {
+    /*pannellum.viewer('pano', {
       "type": "equirectangular",
       "panorama": "../../assets/starmap.jpg",
+      "autoLoad": true,
+      "showControls": false
+    });*/
+    pannellum.viewer('pano', {
+      "type": "multires",
+      "multiRes": {
+          "basePath": "../../assets/panorama",
+          "path": "/%l/%s%x_%y",
+          "fallbackPath": "/fallback/%s",
+          "extension": "png",
+          "tileResolution": 1024,
+          "maxLevel": 0,
+          "cubeResolution": 1024
+      },
       "autoLoad": true,
       "showControls": false
     });
