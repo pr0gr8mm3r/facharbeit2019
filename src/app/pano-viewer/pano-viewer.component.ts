d270bd5581438ed772bc0fb23968dc944ee6b5d1
p_0g_8mm3_ <lachenmon@web.de>
2019-05-09 22:15:44 +0200

    sternenbilder proof of concept

--- src/app/pano-viewer/pano-viewer.component.ts
+++ src/app/pano-viewer/pano-viewer.component.ts
@@ -13,16 +13,49 @@ export class PanoViewerComponent implements OnInit {
 
   ngOnInit() {
     pannellum.viewer('pano', {
-      "type": "multires",
-      "multiRes": {
-          "basePath": "../../assets/panorama/basemap",
-          "path": "/%l/%s%y_%x",
-          "fallbackPath": "/fallback/%s",
-          "extension": "jpg",
-          "tileResolution": 512,
-          "maxLevel": 5,
-          "cubeResolution": 5208
+      "default": {
+        "firstScene": "basemap"
       },
+
+      "scenes": {
+        "basemap": {
+          "title": "Basemap",
+          "type": "multires",
+          "multiRes": {
+            "basePath": "../../assets/panorama/basemap",
+            "path": "/%l/%s%y_%x",
+            "fallbackPath": "/fallback/%s",
+            "extension": "jpg",
+            "tileResolution": 512,
+            "maxLevel": 5,
+            "cubeResolution": 5208
+          },
+          "hotSpots": [
+            {
+              "pitch": 0,
+              "yaw": 0,
+              "type": "scene",
+              "text": "Sternenkartemap",
+              "sceneId": "sternenkartemap"
+            }
+          ]
+        },
+
+        "sternenkartemap": {
+          "title": "Sternenbilderkarte",
+          "type": "multires",
+          "multiRes": {
+            "basePath": "../../assets/panorama/sternenbildermap",
+            "path": "/%l/%s%y_%x",
+            "fallbackPath": "/fallback/%s",
+            "extension": "jpg",
+            "tileResolution": 512,
+            "maxLevel": 5,
+            "cubeResolution": 5208
+          }
+        }
+      },
+
       "autoLoad": true,
       "showControls": false
     });
